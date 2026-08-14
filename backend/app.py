78ff6db9950e2e5972883d46a4867c236dc37c35
revanth-gatla <revanthgatla2006@gmail.com>
2026-08-15 01:08:58 +0530

    Connect FastApi to postgreSQL

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,21 @@
 from fastapi import FastAPI
+from database import get_db_connection
 
 app = FastAPI(title="SWIPE X API")
-
 
 @app.get("/")
 def home():
     return {"message": "SWIPE X Backend"}
+
+@app.get("/test-db")
+def test_db():
+    connection = get_db_connection()
+    cursor = connection.cursor()
+
+    cursor.execute("SELECT current_database();")
+    result = cursor.fetchone()
+
+    cursor.close()
+    connection.close()
+
+    return {"database": result[0]}
