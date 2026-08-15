861dc7dd938f385f8d47b2650f97ff61c8689579
revanth-gatla <revanthgatla2006@gmail.com>
2026-08-15 22:48:30 +0530

    Add candidate registration API

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,13 @@
-from fastapi import FastAPI
+from passlib.context import CryptContext
+from fastapi import FastAPI, HTTPException
 from database import get_db_connection
+from pydantic import EmailStr
 
 app = FastAPI(title="SWIPE X API")
+pwd_context = CryptContext(
+    schemes=["bcrypt"],
+    deprecated="auto"
+)
 
 @app.get("/")
 def home():
@@ -19,3 +25,42 @@ def test_db():
     connection.close()
 
     return {"database": result[0]}
+
+@app.post("/register")
+def register(name: str, email: EmailStr, password: str):
+    connection = get_db_connection()
+    cursor = connection.cursor()
+    cursor.execute(
+        "SELECT id FROM users WHERE email = %s;",
+        (email,))
+    existing_user = cursor.fetchone()
+
+    if existing_user:
+        cursor.close()
+        connection.close()
+        raise HTTPException(
+            status_code=409,
+            detail="Email already registered"
+        )
+
+    hashed_password = pwd_context.hash(password)
+    cursor.execute(
+        """
+        INSERT INTO users (name, email, password)
+        VALUES (%s, %s, %s)
+        RETURNING id;
+        """,
+        (name, email, hashed_password)
+    )
+
+    user_id = cursor.fetchone()[0]
+
+    connection.commit()
+
+    cursor.close()
+    connection.close()
+
+    return {
+        "message": "Registration successful",
+        "user_id": user_id
+    }
